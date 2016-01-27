e27ca7acb0b4bd705359591e2319af7e29e7f25e
Paul Gestwicki <pvgestwicki@bsu.edu>
2016-01-27 11:23:56 -0500

    Use Maven to manage the build
    This allows us to integrate with TravisCI, fixing #1.

---
+++ src/main/java/edu/bsu/storygame/StoryGame.java
@@ -0,0 +1,23 @@
+package edu.bsu.storygame;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class StoryGame extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Spring Studio Project");
+        StackPane root = new StackPane();
+        root.getChildren().add(new Label("Hello, world"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
---
+++ src/test/java/edu/bsu/storygame/SampleTest.java
@@ -0,0 +1,10 @@
+package edu.bsu.storygame;
+
+import org.junit.Test;
+
+public class SampleTest {
+    @Test
+    public  void testConfiguration() {
+        // This test only exists as a placeholder. Once we have real tests, it can be deleted.
+    }
+}
