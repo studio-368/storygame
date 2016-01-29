b5c8c93ba15b7967c7bff3f301e9603c1cdb2ee9
Paul Gestwicki <pvgestwicki@bsu.edu>
2016-01-29 10:17:32 -0500

    Add react.Value sample with Phase change
    This also introduces a game context object, which we talked about in our CRC analysis on 1/19 morning.

--- src/main/java/edu/bsu/storygame/StoryGame.java
+++ src/main/java/edu/bsu/storygame/StoryGame.java
@@ -1,10 +1,14 @@
 package edu.bsu.storygame;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import react.Slot;
 
 public class StoryGame extends Application {
 
@@ -14,12 +18,39 @@ public class StoryGame extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        final GameContext context = new GameContext();
+        PhaseLabel phaseLabel = new PhaseLabel(context);
+        Button button = new Button("Change phase!");
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                if (context.phase.get().equals(Phase.MOVEMENT))
+                    context.phase.update(Phase.ENCOUNTER);
+                else
+                    context.phase.update(Phase.MOVEMENT);
+            }
+        });
+
         primaryStage.setTitle("Spring Studio Project");
-        StackPane root = new StackPane();
+        VBox root = new VBox();
         root.getChildren().add(new Label("Hello, world"));
+        root.getChildren().add(phaseLabel);
+        root.getChildren().add(button);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
 
 
+    }
+
+    private class PhaseLabel extends Label {
+
+        public PhaseLabel(GameContext context) {
+            super(context.phase.get().toString());
+            context.phase.connect(new Slot<Phase>() {
+                @Override
+                public void onEmit(Phase phase) {
+                    PhaseLabel.this.setText(phase.toString());
+                }
+            });
+        }
     }
 }
