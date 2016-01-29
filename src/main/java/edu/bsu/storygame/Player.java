e5709f269039b0ed2082d81761a661fda091436c
Jack McGinnis <jtmcginnis@bsu.edu>
2016-01-29 10:40:03 -0700

    Added placeholder, gridpane, UI and Player class

---
+++ src/main/java/edu/bsu/storygame/Player.java
@@ -0,0 +1,50 @@
+package edu.bsu.storygame;
+
+
+import java.awt.*;
+import java.util.List;
+
+public class Player {
+
+    String name;
+    Color playerColor;
+    List skills;
+    Point position;
+    int totalPoints;
+
+    public Player(String name, Color playerColor, List skills, Point position, int totalPoints){
+        this.name = name;
+        this.playerColor = playerColor;
+        this.skills = skills;
+        this.position = position;
+        this.totalPoints = totalPoints;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Point getPosition() {
+        return position;
+    }
+
+    public int getTotalPoints() {
+        return totalPoints;
+    }
+
+    public Color getPlayerColor() {
+        return playerColor;
+    }
+
+    public List getSkills() {
+        return skills;
+    }
+
+    public void addSkill(String skill){
+        skills.add(skill);
+    }
+
+    public void removeSkill(String skill){
+        skills.remove(skill);
+    }
+}
