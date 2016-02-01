3d0dae413bcc8b109a4b8b5f9f108ec3a52c5900
Jessica Lohse <jessicaalohse@gmail.com>
2016-02-01 10:24:38 -0500

    Create setters for updating of future values

--- src/main/java/edu/bsu/storygame/views/PlayerView.java
+++ src/main/java/edu/bsu/storygame/views/PlayerView.java
@@ -19,33 +19,47 @@ public class PlayerView extends VBox {
 
     Player player;
     Rectangle playerBox = new Rectangle(200, 75);
+    Text name;
+    Text skills;
+    Rectangle diamond = new Rectangle(40, 40);
+    Text points;
+    StackPane pane = new StackPane();
 
     public PlayerView(Player player) {
         this.player = player;
-        String color = player.getPlayerColor().toString();
-        playerBox.setFill(Paint.valueOf(color));
-        StackPane pane = new StackPane();
-        Text name = new Text(0, 0, player.getName());
-        StackPane.setAlignment(name, Pos.CENTER_LEFT);
-        StackPane.setMargin(name, new Insets(0, 0, 10, 10));
-        Text skills = new Text(10, 10, player.getSkillString());
-        StackPane.setAlignment(skills, Pos.BOTTOM_LEFT);
-        StackPane.setMargin(skills, new Insets(10, 10, 10, 10));
-        Rectangle diamond = new Rectangle(40, 40);
+        playerBox.setFill(Paint.valueOf(player.getPlayerColor().toString()));
+        setName();
+        setSkills(player.getSkillString());
         diamond.getTransforms().add(new Rotate(45));
         diamond.setFill(null);
         diamond.setStroke(Color.BLACK);
         StackPane.setAlignment(diamond, Pos.CENTER_RIGHT);
         StackPane.setMargin(diamond, new Insets(0, -10, 20, 0));
-        Text points = new Text(Integer.toString(player.getTotalPoints()));
-        StackPane.setAlignment(points, Pos.CENTER_RIGHT);
-        StackPane.setMargin(points, new Insets(0, 25, 0, 0));
+        setPoints(Integer.toString(player.getTotalPoints()));
         pane.getChildren().addAll(playerBox, name, skills, diamond, points);
         this.getChildren().add(0, pane);
     }
 
     public VBox getView() {
         return this;
+    }
+
+    public void setName(){
+        name = new Text(0, 0, player.getName());
+        StackPane.setAlignment(name, Pos.CENTER_LEFT);
+        StackPane.setMargin(name, new Insets(0, 0, 10, 10));
+    }
+
+    public void setSkills(String skillString) {
+        skills = new Text(10, 10, skillString);
+        StackPane.setAlignment(skills, Pos.BOTTOM_LEFT);
+        StackPane.setMargin(skills, new Insets(10, 10, 10, 10));
+    }
+
+    public void setPoints(String pointString){
+        points = new Text(pointString);
+        StackPane.setAlignment(points, Pos.CENTER_RIGHT);
+        StackPane.setMargin(points, new Insets(0, 25, 0, 0));
     }
 
 }
