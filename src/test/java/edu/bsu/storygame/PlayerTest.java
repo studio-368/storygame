a1b48f15d43e0b10fa70a12a6dd26d78827be728
Jack <jtmcginnis@bsu.edu>
2016-02-02 18:03:59 -0700

    UI changed to CreatePlayerView StoryGame is now the master UI CreatePlayerView and MapView are accessed from StoryGame Player Constructor is Fixed

--- src/test/java/edu/bsu/storygame/PlayerTest.java
+++ src/test/java/edu/bsu/storygame/PlayerTest.java
@@ -7,8 +7,22 @@ public class PlayerTest {
 
     @Test
     public void testSetRegion(){
-        Player player = new Player();
+        Player player = new Player(null,null,null,null,0);
         player.setRegion("New Africa");
         Assert.assertEquals("New Africa", player.getRegion());
+    }
+
+    @Test
+    public void testAddPoints(){
+        Player player = new Player(null,null,null,null,0);
+        player.addPoints(2);
+        Assert.assertEquals(2, player.getTotalPoints());
+    }
+
+    @Test
+    public void testRemovePoints(){
+        Player player = new Player(null,null,null,null,2);
+        player.removePoints(2);
+        Assert.assertEquals(0, player.getTotalPoints());
     }
 }
