05af8ed73491d0856510dc287f50d415f85842c5
Austin "Paco" DeArmond <hello@paco.audio>
2016-02-05 14:36:52 -0500

    Create Encounter class

---
+++ src/test/java/edu/bsu/storygame/EncounterTest.java
@@ -0,0 +1,43 @@
+package edu.bsu.storygame;
+
+import javafx.scene.image.Image;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+public class EncounterTest {
+
+    private static final Image IMAGE_TEST = mock(Image.class);
+    private static final Encounter ENCOUNTER_TEST = Encounter.builder()
+            .setMonsterName("a wraith")
+            .setMonsterImage(IMAGE_TEST)
+            .setRegion("Europe")
+            .addReaction("Run")
+            .addReaction("Converse")
+            .addReaction("Question")
+            .addReaction("Attack")
+            .build();
+
+    @Test
+    public void testEncounterNameIsWraith() {
+        assertEquals("a wraith", ENCOUNTER_TEST.getMonsterName());
+    }
+
+    @Test
+    public void testEncounterMonsterImageIsWraithTemp() {
+        assertEquals(IMAGE_TEST, ENCOUNTER_TEST.getMonsterImage());
+    }
+
+    @Test
+    public void testEncounterRegionIsEurope() {
+        assertEquals("Europe", ENCOUNTER_TEST.getRegion());
+    }
+
+    @Test
+    public void testEncounterReactionsAreInOrder() {
+        String[] testReactions = {"Run", "Converse", "Question", "Attack"};
+        assertArrayEquals(testReactions, ENCOUNTER_TEST.getReactions());
+    }
+}
