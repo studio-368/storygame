563d21a90a5466ec1aba7d658cbf94dd657ae7c9
Brennan Wade <bjwade@bsu.edU>
2016-02-09 11:22:06 -0500

    fix storyView from not appearing

--- src/main/java/edu/bsu/storygame/views/EncounterView.java
+++ src/main/java/edu/bsu/storygame/views/EncounterView.java
@@ -81,7 +81,7 @@ public class EncounterView extends VBox {
     @FXML
     public void onConfirm(ActionEvent actionEvent) {
         confirmButton.setOnAction(event -> {
-            context.phase.update(Phase.MOVEMENT);
+            context.phase.update(Phase.STORY);
         });
     }
 
