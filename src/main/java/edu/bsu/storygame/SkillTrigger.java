f74d7fe1aa0392a65d296e67ec04ad09ef593414
Austin Paco DeArmond <hello@paco.audio>
2016-02-09 13:24:44 -0500

    Create SkillTrigger with Skill and narrative

---
+++ src/main/java/edu/bsu/storygame/SkillTrigger.java
@@ -0,0 +1,12 @@
+package edu.bsu.storygame;
+
+public class SkillTrigger {
+
+    public final Skill skill;
+    public final String narrative;
+
+    public SkillTrigger(Skill skill, String narrative) {
+        this.skill = skill;
+        this.narrative = narrative;
+    }
+}
